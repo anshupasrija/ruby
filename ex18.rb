a5669696f99a8df94328d9d9f52aee7da412d223
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-10 16:04:21 -0700

    benchmark

---
+++ ex18.rb
@@ -0,0 +1,16 @@
+def benchmark
+  start_time = Time.now
+  puts "this is line 2" 
+  yield
+  end_time = Time.now
+  puts "this is end time"
+  running_time = end_time - start_time
+  puts "this is running time"
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+ 
+running_time = benchmark { long_string.reverse } 
+puts "string.reverse took #{running_time} seconds to run"
