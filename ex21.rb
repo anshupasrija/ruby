1626880d5ec94ebd409aeec2af8ac7acc9e50e72
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-10-22 23:16:16 -0700

    ruby class method

---
+++ ex21.rb
@@ -0,0 +1,15 @@
+At the bottom of this file, create a Calculator class.
+
+Calculator#sum should have one instance method called sum.
+
+It should take in two arguments and return their sum, plain and simple.
+
+CHALLENGE00
+class Calculator
+  def sum(num1,num2)
+     num1 + num2
+  end
+end
+
+calculator = Calculator.new
+puts calculator.sum(2, 3)
